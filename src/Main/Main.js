287a69599d2e921de01773fd873449f16bc57a32
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-04-25 14:27:54 -0800

    got names rendering

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import backgroundImg from '../background.png';
 import './Main.css';
+import '../ZodiacCard/ZodiacCard.css';
+import ZodiacCard from '../ZodiacCard/ZodiacCard.js';
 
-export default function Main() {
+export default function Main(props) {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {props.array.map(prop => 
+        <ZodiacCard className='.zodiac-card'
+          key={prop.name}
+          name={prop.name}
+          dates={prop.dates}
+        />)
+      }
+      
       {/* import the ZodiacCard component */}
       {/* get the zodiac array from props */}
       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,12 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard() {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+export default function ZodiacCard(props) {
+  return (
+    <div>
+      {props.name} born {props.date}
+      <img src={`url(.../public/images/leo.png)`}/>
+
+    </div>
+  );
+  //{/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
 }
